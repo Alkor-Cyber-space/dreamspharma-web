96f9708a312ae6087915bd8f1b91de43e6fc895a
AlkorCyberSpace <info@alkor.in>
2026-04-11 17:36:58 +0530

    Update settings.py

--- dreamspharma/settings.py
+++ dreamspharma/settings.py
@@ -24,10 +24,12 @@ SECRET_KEY = 'django-insecure-v9div(*7#85s3qa+mt-wc%&&57s0sz5bpx4i-(h&)li__-1r*+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+CORS_ALLOW_CREDENTIALS = True
 ALLOWED_HOSTS = ['*']
 
-
+CORS_ALLOWED_ORIGINS = [
+    "https://dreamspharma-web.vercel.app",
+]
 # Application definition
 
 INSTALLED_APPS = [

fe82c3685adebacd05963675ac0239048551881a
AlkorCyberSpace <info@alkor.in>
2026-04-11 17:10:32 +0530

    Update server and media URLs to ngrok

--- frontend/src/services/serverUrl.js
+++ frontend/src/services/serverUrl.js
@@ -1,2 +1,2 @@
-export const serverUrl = "http://127.0.0.1:8000/api/"
-export const mediaUrl = "http://127.0.0.1:8000"
+export const serverUrl = "https://abc.ngrok-free.app/api/"
+export const mediaUrl = "https://abc.ngrok-free.app"

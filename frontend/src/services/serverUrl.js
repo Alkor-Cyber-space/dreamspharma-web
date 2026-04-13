87abfdb49609d10f627981dbe893d6b68a3edb4e
AlkorCyberSpace <info@alkor.in>
2026-04-13 13:54:29 +0530

    Update serverUrl.js

--- frontend/src/services/serverUrl.js
+++ frontend/src/services/serverUrl.js
@@ -1,2 +1,2 @@
 export const serverUrl = "https://dreamspharma-web-1-dpjy.onrender.com/api/"
-export const mediaUrl = "https://dreamspharma-web-1-dpjy.onrender.com/"
+export const mediaUrl = "https://dreamspharma-web-1-dpjy.onrender.com"
